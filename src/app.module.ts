e31d53f6a2e9f0c3b9e33a90a97840ef4ae62d57
Bilanox1 <aurabilanox@gmail.com>
2025-02-04 11:14:47 +0000

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,17 @@ import { PharmacyModule } from './pharmacy/pharmacy.module';
 
 @Module({
   imports: [
-   
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    FavoriteModule,
+    PharmacyModule,
   ],
   controllers: [AppController],
   providers: [AppService],
