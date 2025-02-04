7738e11131a7f7745bb26b54622df387ecf6b184
Bilanox1 <aurabilanox@gmail.com>
2025-02-04 11:19:36 +0000

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,13 +10,6 @@ import { PharmacyModule } from './pharmacy/pharmacy.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        uri: "mongodb+srv://aurabilanox:jbzbZxwKG280cBEY@cluster0.hhcri.mongodb.net/pharmacy",
-      }),
-      inject: [ConfigService],
-    }),
     AuthModule,
     FavoriteModule,
     PharmacyModule,
